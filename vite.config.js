ba284ae11274079938eb377667d2a352316d9631
Eder Bragança <eder.braganca@aluno.ufop.edu.br>
2025-02-05 21:08:00 -0300

    Update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '10.20.20.28',
-    port: 1313,
-  }
+  plugins: [react()]
 })
